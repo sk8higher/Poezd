5a835d08a9ede98e8cc6312745de6c22a07ee385
nellykim <sk8high3r@ya.ru>
2023-06-09 15:58:22 +0300

    add second schedule

--- Poezd/Form1.Designer.cs
+++ Poezd/Form1.Designer.cs
@@ -43,6 +43,17 @@
             label11 = new Label();
             label12 = new Label();
             label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            label20 = new Label();
+            label21 = new Label();
+            label22 = new Label();
+            label23 = new Label();
+            radioButton2 = new RadioButton();
             SuspendLayout();
             // 
             // label1
@@ -97,6 +108,7 @@
             radioButton1.ForeColor = Color.Green;
             radioButton1.Location = new Point(32, 144);
             radioButton1.Name = "radioButton1";
+            radioButton1.Padding = new Padding(5, 0, 0, 0);
             radioButton1.Size = new Size(448, 32);
             radioButton1.TabIndex = 4;
             radioButton1.TabStop = true;
@@ -219,11 +231,151 @@
             label13.TabIndex = 14;
             label13.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // label14
+            // 
+            label14.BackColor = SystemColors.InactiveCaption;
+            label14.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label14.ForeColor = Color.Black;
+            label14.Location = new Point(392, 328);
+            label14.Name = "label14";
+            label14.Size = new Size(80, 23);
+            label14.TabIndex = 25;
+            label14.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            label15.BackColor = SystemColors.InactiveCaption;
+            label15.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label15.ForeColor = Color.Black;
+            label15.Location = new Point(304, 328);
+            label15.Name = "label15";
+            label15.Size = new Size(72, 23);
+            label15.TabIndex = 24;
+            label15.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label16
+            // 
+            label16.BackColor = SystemColors.InactiveCaption;
+            label16.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label16.ForeColor = Color.Black;
+            label16.Location = new Point(224, 328);
+            label16.Name = "label16";
+            label16.Size = new Size(72, 23);
+            label16.TabIndex = 23;
+            label16.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label17
+            // 
+            label17.BackColor = SystemColors.InactiveCaption;
+            label17.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label17.ForeColor = Color.Black;
+            label17.Location = new Point(144, 328);
+            label17.Name = "label17";
+            label17.Size = new Size(64, 23);
+            label17.TabIndex = 22;
+            label17.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label18
+            // 
+            label18.BackColor = SystemColors.InactiveCaption;
+            label18.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label18.ForeColor = Color.Black;
+            label18.Location = new Point(40, 328);
+            label18.Name = "label18";
+            label18.Size = new Size(80, 23);
+            label18.TabIndex = 21;
+            label18.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label19
+            // 
+            label19.BackColor = SystemColors.InactiveCaption;
+            label19.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label19.ForeColor = Color.Red;
+            label19.Location = new Point(384, 296);
+            label19.Name = "label19";
+            label19.Size = new Size(96, 23);
+            label19.TabIndex = 20;
+            label19.Text = "Saint Denis";
+            label19.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label20
+            // 
+            label20.BackColor = SystemColors.InactiveCaption;
+            label20.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label20.ForeColor = Color.Red;
+            label20.Location = new Point(304, 296);
+            label20.Name = "label20";
+            label20.Size = new Size(72, 23);
+            label20.TabIndex = 19;
+            label20.Text = "Emerald";
+            label20.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label21
+            // 
+            label21.BackColor = SystemColors.InactiveCaption;
+            label21.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label21.ForeColor = Color.Red;
+            label21.Location = new Point(224, 296);
+            label21.Name = "label21";
+            label21.Size = new Size(72, 23);
+            label21.TabIndex = 18;
+            label21.Text = "Valentine";
+            label21.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label22
+            // 
+            label22.BackColor = SystemColors.InactiveCaption;
+            label22.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label22.ForeColor = Color.Red;
+            label22.Location = new Point(144, 296);
+            label22.Name = "label22";
+            label22.Size = new Size(72, 23);
+            label22.TabIndex = 17;
+            label22.Text = "Rhodes";
+            label22.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label23
+            // 
+            label23.BackColor = SystemColors.InactiveCaption;
+            label23.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label23.ForeColor = Color.Red;
+            label23.Location = new Point(32, 296);
+            label23.Name = "label23";
+            label23.Size = new Size(104, 23);
+            label23.TabIndex = 16;
+            label23.Text = "Saint Denis";
+            label23.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // radioButton2
+            // 
+            radioButton2.BackColor = SystemColors.InactiveCaption;
+            radioButton2.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton2.ForeColor = Color.Green;
+            radioButton2.Location = new Point(32, 256);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Padding = new Padding(5, 0, 0, 0);
+            radioButton2.Size = new Size(448, 32);
+            radioButton2.TabIndex = 15;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Saint Denis - Rhodes - Valentine - Emerald - Saint Denis";
+            radioButton2.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(606, 845);
+            Controls.Add(label14);
+            Controls.Add(label15);
+            Controls.Add(label16);
+            Controls.Add(label17);
+            Controls.Add(label18);
+            Controls.Add(label19);
+            Controls.Add(label20);
+            Controls.Add(label21);
+            Controls.Add(label22);
+            Controls.Add(label23);
+            Controls.Add(radioButton2);
             Controls.Add(label13);
             Controls.Add(label12);
             Controls.Add(label11);
@@ -262,5 +414,16 @@
         private Label label11;
         private Label label12;
         private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Label label20;
+        private Label label21;
+        private Label label22;
+        private Label label23;
+        private RadioButton radioButton2;
     }
 }
